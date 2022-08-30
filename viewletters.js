150b48f8a11c8e01df947f4f3f8e71a5492b68c4
juanscarlata2 <56403767+juanscarlata2@users.noreply.github.com>
2022-08-29 19:53:27 -0500

    Update viewletters.js

--- viewletters.js
+++ viewletters.js
@@ -1,4 +1,4 @@
-fetch("http://127.0.0.1:80/message/3").then((r) => {
+fetch("http://209.97.177.45:32249/message/4").then((r) => {
     return r.text();
 }).then((x) => {
     fetch("http://127.0.0.1:80/submit", {
